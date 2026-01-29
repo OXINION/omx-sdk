01ebc65d0f74a9b7ec131a43193fc833f7c7cddb
Brandon Ha <brandonha@openmile.co.kr>
2026-01-30 07:46:51 +0900

    fix

--- examples/python/geotrigger-example.py
+++ examples/python/geotrigger-example.py
@@ -1,24 +1,29 @@
 import os
+import asyncio
 from omx_sdk import OMXClient
 
-omx = OMXClient(
-    client_id=os.getenv("OMX_CLIENT_ID"),
-    secret_key=os.getenv("OMX_SECRET_KEY")
-)
+async def main():
+    omx = OMXClient(
+        client_id=os.getenv("OMX_CLIENT_ID"),
+        secret_key=os.getenv("OMX_SECRET_KEY")
+    )
 
-geo_trigger = await omx.geo_trigger.create({
-    "name": "Coffee Shop Promo",
-    "location": {
-        "lat": 43.6532,
-        "lng": -79.3832
-    },
-    "radius": 100,
-    "on_enter": {
-        "notification": {
-            "title": "Welcome!",
-            "body": "Get 30% off your order!"
+    geo_trigger = await omx.geo_trigger.create({
+        "name": "Coffee Shop Promo",
+        "location": {
+            "lat": 43.6532,
+            "lng": -79.3832
+        },
+        "radius": 100,
+        "on_enter": {
+            "notification": {
+                "title": "Welcome!",
+                "body": "Get 30% off your order!"
+            }
         }
-    }
-})
+    })
 
-print("GeoTrigger created:", geo_trigger.id)
+    print("GeoTrigger created:", geo_trigger.id)
+
+if __name__ == "__main__":
+    asyncio.run(main())
